cc124daafd34c175db7c867bb0bd57d3c509f4de
Bikram Rongpi <bikramrongpi088@gmail.com>
2025-01-24 16:43:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def scrape_table_data():
         #         value = html.unescape(cell.text.strip())
         #         row_data.append("NULL" if value == "" else value)
         #     table_data.append(", ".join(row_data))
-        table_data = ["b6r4f6fc-c8ef-4e4d-97f1-291c4f18a9d3, 1128000993, 1, FD 3971 Afd101 K°l Ophµng, Trailer, 1EtqEOAXuHuWEV00000000, 7A2, da_DK, 4, NULL",
+        table_data = ["g6r4f6fc-c8ef-4e4d-97f1-291c4f18a9d3, 1128000993, 1, FD 3971 Afd101 K°l Ophµng, Trailer, 1EtqEOAXuHuWEV00000000, 7A2, da_DK, 4, NULL",
         "98a5fb2a-728f-44e5-839f-16e44c4e6ff4, 1128001343, 1, CU 93 552 Afd105 Kran, Vehicle, 3RjFWcAZmHuWIV00000000, 7A2, da_DK, 4, NULL",
         "5b815c19-41ca-4c37-a2fa-b697e458bab0, 1129000173, 3, CP 5647 Afd101 K°l SideÕbner, Trailer, 0LVtt4AhyHuWMV00000000, 7A2, da_DK, 4, NULL",
         "f64132cf-ee95-44a7-aa6b-1bd69b90d7fb, 1129000183, 2, FD 1644 Afd101 K°l Ophµng, Trailer, 2m4_$SAhyHuWMV00000000, 7A2, da_DK, 4, NULL"]
