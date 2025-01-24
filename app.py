b909b361875f20b857eecb1e7bb16049fbd488aa
Bikram Rongpi <bikramrongpi088@gmail.com>
2025-01-24 16:54:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def insert_into_database(table_data):
             
             # SQL insert statement with correct column order
             sql = """
-            INSERT INTO Gsgroup_backup.dbo.units 
+            INSERT INTO Gsgroup_backup.dbo.units01 
             (ID, Number, Sequence, Name, Description, GUID, Model, Locale, Subscription, Timezone)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             """
